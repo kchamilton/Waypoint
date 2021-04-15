cbadb62568bab69916a808d50524d2743125b3b2
Kayla Hamilton <kaylahamilton@Kaylas-MacBook-Pro.local>
2021-04-15 15:23:06 -0500

    removing problem code

--- waypoint_app/ARKitPersistence/LoginViewController.swift
+++ waypoint_app/ARKitPersistence/LoginViewController.swift
@@ -3,12 +3,5 @@ import UIKit
 class LoginViewController: UIViewController {
 //    @IBOutlet weak var userLoginButton: UIButton!
 //    @IBOutlet weak var adminLoginButton: UIButton!
-    @IBOutlet weak var username: UITextField!
-    @IBOutlet weak var password: UITextField!
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        textField.resignFirstResponder()
-        return true
-    }
 }
 
